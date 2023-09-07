5ab9c6aa1cb89628a87d37b50758b74d95aefe06
barakatimothy <timothybaraka39@gmail.com>
2023-09-07 23:35:50 +0300

    complete

--- portfolio_tracker/cli.py
+++ portfolio_tracker/cli.py
@@ -1,128 +1,147 @@
-# portfolio_tracker/cli.py
-import click
+import argparse
 from database import init_db
 from models import User, Portfolio, Asset
 
-@click.group()
-def cli():
-    pass
+# Create a global database session
+db_session = init_db('sqlite:///portfolio.db')
 
-@cli.command()
-@click.option('--db-uri', type=str, default='sqlite:///portfolio.db', help='Database URI')
-def init_db(db_uri):
-    """Initialize the database."""
-    init_db(db_uri)
-    click.echo('Database initialized.')
-
-
-
-# Create a new user
-@cli.command()
-@click.argument('username')
-@click.option('--db-uri', default='sqlite:///portfolio.db', help='Database URI')
-def create_user(username, db_uri):
+def create_user(args):
     """Create a new user."""
+    username = args.username
+    db_uri = args.db_uri
+
     session = init_db(db_uri)
 
     # Data Validation: Validate the username
     if not username:
-        click.echo("Error: Username cannot be empty.")
+        print("Error: Username cannot be empty.")
         return  # Exit the command
 
     # Check if the username already exists
     existing_user = session.query(User).filter_by(username=username).first()
     if existing_user:
-        click.echo(f"Error: Username '{username}' already exists.")
+        print(f"Error: Username '{username}' already exists.")
         return  # Exit the command
 
     # Confirmation Prompt: Ask the user for confirmation
-    confirmation = click.confirm(f'Create user "{username}"?')
-    if not confirmation:
-        click.echo("User creation canceled.")
+    confirmation = input(f'Create user "{username}"? (yes/no): ')
+    if confirmation.lower() != 'yes':
+        print("User creation canceled.")
         return  # Exit the command
 
     # Create the user
     user = User(username=username)
     session.add(user)
     session.commit()
-    click.echo(f'User "{username}" created.')
-
-
-
+    print(f'User "{username}" created.')
     
-# Create a new portfolio
-@cli.command()
-@click.argument('user_id', type=int)
-@click.argument('name')
-@click.option('--db-uri', default='sqlite:///portfolio.db', help='Database URI')
-def create_portfolio(user_id, name, db_uri):
-    """Create a new portfolio."""
+# Define the function to create a new portfolio
+def create_portfolio(args):
+    user_id = args.user_id
+    name = args.name
+    db_uri = args.db_uri
+
     session = init_db(db_uri)
 
     # Data Validation: Validate user_id and name
     if not name:
-        click.echo("Error: Portfolio name cannot be empty.")
+        print("Error: Portfolio name cannot be empty.")
         return  # Exit the command
 
     # Check if the user exists
     user = session.query(User).filter_by(id=user_id).first()
     if not user:
-        click.echo(f"Error: User with ID {user_id} does not exist.")
+        print(f"Error: User with ID {user_id} does not exist.")
         return  # Exit the command
 
     # Confirmation Prompt: Ask the user for confirmation
-    confirmation = click.confirm(f'Create portfolio "{name}" for user ID {user_id}?')
-    if not confirmation:
-        click.echo("Portfolio creation canceled.")
+    confirmation = input(f'Create portfolio "{name}" for user ID {user_id}? (yes/no): ')
+    if confirmation.lower() != 'yes':
+        print("Portfolio creation canceled.")
         return  # Exit the command
 
     # Create the portfolio
     portfolio = Portfolio(user_id=user_id, name=name)
     session.add(portfolio)
     session.commit()
-    click.echo(f'Portfolio "{name}" created for user ID {user_id}.')
+    print(f'Portfolio "{name}" created for user ID {user_id}.')
+    
+    
+# Define the function to create a new asset
+def create_asset(args):
+    portfolio_id = args.portfolio_id
+    symbol = args.symbol
+    quantity = args.quantity
+    db_uri = args.db_uri
 
-
-
-
-# Create a new asset
-@cli.command()
-@click.argument('portfolio_id', type=int)
-@click.argument('symbol')
-@click.argument('quantity', type=int)
-@click.option('--db-uri', default='sqlite:///portfolio.db', help='Database URI')
-def create_asset(portfolio_id, symbol, quantity, db_uri):
-    """Create a new asset."""
     session = init_db(db_uri)
 
     # Data Validation: Validate portfolio_id, symbol, and quantity
     if not symbol:
-        click.echo("Error: Asset symbol cannot be empty.")
+        print("Error: Asset symbol cannot be empty.")
         return  # Exit the command
 
     if quantity <= 0:
-        click.echo("Error: Quantity must be greater than zero.")
+        print("Error: Quantity must be greater than zero.")
         return  # Exit the command
 
     # Check if the portfolio exists
     portfolio = session.query(Portfolio).filter_by(id=portfolio_id).first()
     if not portfolio:
-        click.echo(f"Error: Portfolio with ID {portfolio_id} does not exist.")
+        print(f"Error: Portfolio with ID {portfolio_id} does not exist.")
         return  # Exit the command
 
     # Confirmation Prompt: Ask the user for confirmation
-    confirmation = click.confirm(f'Add asset "{symbol}" to portfolio ID {portfolio_id}?')
-    if not confirmation:
-        click.echo("Asset creation canceled.")
+    confirmation = input(f'Add asset "{symbol}" to portfolio ID {portfolio_id}? (yes/no): ')
+    if confirmation.lower() != 'yes':
+        print("Asset creation canceled.")
         return  # Exit the command
 
     # Create the asset
     asset = Asset(portfolio_id=portfolio_id, symbol=symbol, quantity=quantity)
     session.add(asset)
     session.commit()
-    click.echo(f'Asset "{symbol}" added to portfolio ID {portfolio_id}.')
+    print(f'Asset "{symbol}" added to portfolio ID {portfolio_id}.')
+
+def main():
+    parser = argparse.ArgumentParser(description="Portfolio Tracker CLI")
+    subparsers = parser.add_subparsers()
+
+    # Create subparser for the 'create-user' command
+    create_user_parser = subparsers.add_parser("create-user")
+    create_user_parser.add_argument("username", type=str, help="Username")
+    create_user_parser.add_argument("--db-uri", type=str, default="sqlite:///portfolio.db", help="Database URI")
+    create_user_parser.set_defaults(func=create_user)
+
+    # Create subparser for the 'create-portfolio' command
+    create_portfolio_parser = subparsers.add_parser("create-portfolio")
+    create_portfolio_parser.add_argument("user_id", type=int, help="User ID")
+    create_portfolio_parser.add_argument("name", type=str, help="Portfolio Name")
+    create_portfolio_parser.add_argument("--db-uri", type=str, default="sqlite:///portfolio.db", help="Database URI")
+    create_portfolio_parser.set_defaults(func=create_portfolio)
+
+    # Create subparser for the 'create-asset' command
+    create_asset_parser = subparsers.add_parser("create-asset")
+    create_asset_parser.add_argument("portfolio_id", type=int, help="Portfolio ID")
+    create_asset_parser.add_argument("symbol", type=str, help="Asset Symbol")
+    create_asset_parser.add_argument("quantity", type=int, help="Asset Quantity")
+    create_asset_parser.add_argument("--db-uri", type=str, default="sqlite:///portfolio.db", help="Database URI")
+    create_asset_parser.set_defaults(func=create_asset)
+
+    args = parser.parse_args()
+    if hasattr(args, "func"):
+        args.func(args)
+
+
+if __name__ == "__main__":
+    main()
 
 
 
-if __name__ == '__main__':
-    cli()
+
+
+
+
+
+
+
